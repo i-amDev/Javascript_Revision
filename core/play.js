0864b22eb777d4412e9e17695371c484bcc54443
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-10 12:03:38 +0530

    - Learned about functions, taking parameters as inputs and then accessing them while calling it.

--- core/play.js
+++ core/play.js
@@ -8,3 +8,9 @@ console.log('Hello JavaScript!');
 console.log(name);
 console.log(age);
 console.log(hasHobbies);
+
+function summarizeUser(userName, userAge, userHobby) {
+    return "Name is " + userName + " Age is " + userAge + " and the user has hobbies " + userHobby;
+}
+
+console.log(summarizeUser("Tony", 50, false));
