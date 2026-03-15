63d5475e623eced755caa339fc7f6696ecd6571a
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-03-15 19:11:44 +0530

    - Refactored.

--- core/play.js
+++ core/play.js
@@ -53,6 +53,7 @@ for (let temp of hobbies) {
 console.log(hobbies.map(hobby => "Hobby : " + hobby));
 console.log(hobbies);
 
+
 // slice operator
 const copiedArray = hobbies.slice();
 console.log(copiedArray);
