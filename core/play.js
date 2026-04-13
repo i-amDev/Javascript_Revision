cbde12549752fca260982c9d5c3f0f0e9686e1db
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-04-13 15:52:29 +0530

    - Refactored.

--- core/play.js
+++ core/play.js
@@ -13,7 +13,6 @@ function summarizeUser(userName, userAge, userHobby) {
     return "Name is " + userName + " Age is " + userAge + " and the user has hobbies " + userHobby;
 }
 
-
 console.log(summarizeUser("Tony", 50, false));
 
 
