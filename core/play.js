0965a2fc7a7dcdfc293a102f2c8c3cc4aa161903
i-amDev <92856531+i-amDev@users.noreply.github.com>
2026-05-24 11:14:41 +0530

    - Refactored.

--- core/play.js
+++ core/play.js
@@ -19,7 +19,6 @@ const summarizeUserInConstant = function(userName, userAge, userHobby) {
     return "Name is " + userName + " Age is " + userAge + " and the user has hobbies " + userHobby;
 }
 
-
 console.log(summarizeUserInConstant("Steve", 80, true));
 
 const arrowFunction = (userName, userAge, userHobby) => {
